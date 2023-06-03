70a8311c1eaecb964bc31f4ce6bb6c70e5fca870
Lucca Barroso <barrosolucca@gmail.com>
2023-06-03 11:35:22 -0300

    ajuste middleware auth

--- routes/requireLoginMiddleware.js
+++ routes/requireLoginMiddleware.js
@@ -1,9 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const requireLogin = (req, res, next) => {
-  const reqToken = req.headers.authorization;
-  if (!reqToken) return res.status(401).send("No token provided.");
-  const token = req.headers.authorization.split(" ")[1];
+  const token = req.cookies.accessToken;
   if(!token) return res.status(401).send("No token provided.");
   jwt.verify(token, process.env.ACCESS_TOKEN_PRIVATE_KEY, (err, user) => {
     if (err) {
